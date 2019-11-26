e08244592e66c8a6a678d9e207b63695ccd5630b
TakahiroSato <futurenova52@gmail.com>
2019-11-26 07:08:19 +0000

    bisyusei

--- fish/install.sh
+++ fish/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-#cp fishfile ~/.config/fish/
-#cp functions/* ~/.config/fish/functions
-
 ln -sf `pwd`/fishfile ~/.config/fish/fishfile
 for f in `ls functions`
 do
