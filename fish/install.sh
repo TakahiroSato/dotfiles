5f2a7eade1d432327eeef43de762cddd4800a24f
TakahiroSato <futurenova52@gmail.com>
2020-11-02 17:02:16 +0900

    install fish

--- fish/install.sh
+++ fish/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd `dirname $0`
+
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
+
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 ln -sf `pwd`/fishfile ~/.config/fish/fishfile
 ln -sf `pwd`/config.fish ~/.config/fish/config.fish
