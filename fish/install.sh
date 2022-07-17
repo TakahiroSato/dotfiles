c0806af2c38dba173f83dc607f9dcdec14eafbe6
TakahiroSato <futurenova52@gmail.com>
2022-07-17 16:03:04 +0900

    fisherの更新対応

--- fish/install.sh
+++ fish/install.sh
@@ -6,11 +6,11 @@ sudo apt-get update
 sudo apt-get install fish
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-ln -sf `pwd`/fishfile ~/.config/fish/fishfile
+ln -sf `pwd`/fish_plugins ~/.config/fish/fish_plugins
 ln -sf `pwd`/config.fish ~/.config/fish/config.fish
 for f in `ls functions`
 do
   ln -sf `pwd`/functions/$f ~/.config/fish/functions/$f
 done
 
-cat fishfile | fish -c fisher install
+cat fish_plugins | fish -c fisher install
